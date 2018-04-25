56148706dd046b6c7132a26ea76a8e4421f4006e
Ethan <e.gates42@gmail.com>
2018-04-25 14:38:46 -0400

    upload cask formula

---
+++ vrecord-gui.rb
@@ -0,0 +1,10 @@
+cask 'vrecord-gui' do
+  version 'v2018-04-24'
+  sha256 '49a786f5c3b3da098fa69296eb5cd75e37946e387da435032018e79da7102d66'
+  url "https://github.com/EG-tech/vrecord-gui/archive/v2018-04-24.zip"
+
+  homepage "https://github.com/amiaopensource/vrecord"
+
+  app 'vrecord-gui-2018-04-24/vrecord.app'
+
+end
